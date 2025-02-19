089a63ef9a56464fefd24480fcfd582f54349630
Salaledd6 <aleksisalin03@gmail.com>
2025-02-19 12:56:04 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const cards = document.querySelectorAll(".card");
+
+    cards.forEach(card => {
+        card.addEventListener("click", function() {
+            card.classList.toggle("flipped");
+        });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleButton = document.getElementById("theme-toggle");
+    const body = document.body;
+
+    // tarkistaa localstoragesta teeman 
+    if (localStorage.getItem("theme") === "light") {
+        body.classList.add("light-mode");
+    }
+
+    toggleButton.addEventListener("click", function () {
+        body.classList.toggle("light-mode");
+
+        // tallentaa teeman logalstorageen
+        if (body.classList.contains("light-mode")) {
+            localStorage.setItem("theme", "light");
+        } else {
+            localStorage.setItem("theme", "dark");
+        }
+    });
+});
